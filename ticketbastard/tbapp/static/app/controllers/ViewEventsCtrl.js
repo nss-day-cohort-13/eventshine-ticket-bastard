603dd5b71e27672561da1f968da1183a3e394d06
slick <slick@ardipithecus.local>
2016-08-31 12:26:53 -0500

    add remaining tickets to view events page

--- ticketbastard/tbapp/static/app/controllers/ViewEventsCtrl.js
+++ ticketbastard/tbapp/static/app/controllers/ViewEventsCtrl.js
@@ -1,15 +1,28 @@
-app.controller("ViewEventsCtrl", function($http, $uibModal) {
+app.controller("ViewEventsCtrl", function($http, $uibModal, $timeout) {
     const viewEvents = this;
-
-    $http.get("http://localhost:8000/tbapp/events")
-    .then((response) => viewEvents.events = response.data);
 
     $http.get("http://localhost:8000/tbapp/venues")
     .then((response) => viewEvents.venues = response.data);
 
+    $http.get("http://localhost:8000/tbapp/tickets")
+    .then((response) => viewEvents.all_tickets = response.data)
+    .then(() => {
+      $http.get("http://localhost:8000/tbapp/events")
+      .then((response) => {
+        viewEvents.events = response.data
+        for (var i = 0; i < viewEvents.events.length; i++) {
+          stored_event = viewEvents.events[i]
+          event_tickets_count = viewEvents.getEventTicketCount(stored_event.pk)
+          stored_event.remaining_tickets = (stored_event.fields.max_tickets - event_tickets_count)
+        }
+        $timeout
+      });
+    })
+
     viewEvents.date = new Date().toISOString();
 
     viewEvents.purchase = event => {
+      event.remaining_tickets = event.fields.max_tickets - viewEvents.getEventTicketCount(event)
       const modal = $uibModal.open({
         templateUrl: "../static/app/partials/purchaseTicketsModal.html",
         controller: "PurchaseTicketsModalCtrl",
@@ -29,4 +42,15 @@ app.controller("ViewEventsCtrl", function($http, $uibModal) {
         .catch(err => console.error(err))
       }, () => {});
     };
+
+    viewEvents.getEventTicketCount = event_id => {
+      let total_event_tickets = 0;
+      for (var i = 0; i < viewEvents.all_tickets.length; i++) {
+        const ticket = viewEvents.all_tickets[i]
+        if (ticket.fields.event == event_id) {
+          total_event_tickets ++;
+        }
+      }
+      return total_event_tickets;
+    }
 });
