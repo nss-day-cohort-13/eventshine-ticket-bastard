24006b3b26b1b95ffeca9f1553d199b948f1bb63
pbhowelljr <pbhowelljr@gmail.com>
2016-08-31 23:13:59 -0500

    limit ticket purchase count to remaining ticket count

--- ticketbastard/tbapp/static/app/controllers/PurchaseTicketsModalCtrl.js
+++ ticketbastard/tbapp/static/app/controllers/PurchaseTicketsModalCtrl.js
@@ -1,7 +1,19 @@
-app.controller("PurchaseTicketsModalCtrl", function($uibModalInstance, event) {
+app.controller("PurchaseTicketsModalCtrl", function($uibModalInstance, $timeout, event) {
   const purchase = this;
   purchase.event = event;
-  purchase.confirm = () => { $uibModalInstance.close(purchase.count); };
+  purchase.remaining_ticket_count_warning = false;
+
+  purchase.confirm = () => {
+    if (purchase.count <= purchase.event.remaining_tickets) {
+      $uibModalInstance.close(purchase.count, purchase.event);
+    }
+    else {
+      console.log(purchase.remaining_ticket_count_warning)
+      purchase.remaining_ticket_count_warning = true;
+      $timeout()
+      console.log(purchase.remaining_ticket_count_warning)
+    }
+  };
 
   purchase.cancel = () => { $uibModalInstance.dismiss("cancel"); };
 });
